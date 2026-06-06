e6ae7b34e3e81afc1ffaad0814971981178ba276
Yefrey De la Cruz <yefrey759@gmail.com>
2026-06-05 23:51:08 -0500

    Configurar puerto Render

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ res.json({
 
 });
 
-app.listen(3000,()=>{
-console.log("Servidor corriendo en puerto 3000");
-});
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
