ce41a4a280e26594ba1cb61c2f1a83fc2fc0b066
Yefrey De la Cruz <yefrey759@gmail.com>
2026-06-07 19:39:22 -0500

    api render

--- APP1.js
+++ APP1.js
@@ -2,7 +2,7 @@
 // ======================
 // DATOS
 // ======================
-const API = "https://jefrey.onrender.com/estudiantes";
+const API = "https://tarea1-v57u.onrender.com/estudiantes";
 let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
 
 let editIndex = null;
